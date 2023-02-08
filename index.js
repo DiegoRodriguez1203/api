4875f85a5548a07712f4dae0c98ddca8c81bc0cb
DiegoRodriguez1203 <diegorodriguez1203@gmail.com>
2023-02-08 17:21:47 -0500

    index correct

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 // Create server
 const app = express();
+
+// capture body
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.json())
+
 
 
 app.get('/', (req, res) => {
@@ -12,8 +20,19 @@ app.get('/', (req, res) => {
     })
 });
 
+// Port assign
 
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
     console.log(`servidor andando en: ${PORT}`)
 })
+
+
+// mongo db conection
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@fundacioncrecer.swvdyyt.mongodb.net/${process.env.DBNAME}`;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
